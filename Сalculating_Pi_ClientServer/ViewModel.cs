28457a906354c39714b140336a86cf30b93b355f
Konstantin <turchakkonstantin@gmail.com>
2023-07-31 14:53:40 +0300

    Добавьте файлы проекта.

---
+++ Сalculating_Pi_ClientServer/ViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Сalculating_Pi_ClientServer
+{
+    public class ViewModel
+    {
+        ObservableCollection<Point> points;
+    }
+}
