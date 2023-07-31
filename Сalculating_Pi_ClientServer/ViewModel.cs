ed4eb7209ffde61e32a9d047bb9307622ef18e12
Konstantin <turchakkonstantin@gmail.com>
2023-07-31 16:50:29 +0300

    add command class

--- Сalculating_Pi_ClientServer/ViewModel.cs
+++ Сalculating_Pi_ClientServer/ViewModel.cs
@@ -5,11 +5,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Media;
 
 namespace Сalculating_Pi_ClientServer
 {
     public class ViewModel
     {
-        ObservableCollection<Point> points;
+        public ObservableCollection<Point> Points { get; set; }
+        public Colors StatusColor { get; set; }
+        public string Pi { get; set; }
+
+        public ViewModel()
+        {
+            Points = new ObservableCollection<Point>();
+        }
+
+        RelayCommand m_connectCommand;
+
+        public RelayCommand ConnectCommand
+        {
+            get
+            {
+                return m_connectCommand ??
+                    (m_connectCommand = new RelayCommand(obj =>
+                    {
+                        Application.Current.Dispatcher.InvokeAsync(() =>
+                        {
+                            ConnectToServer();
+                        });
+                    }));
+            }
+        }
+
+        private void ConnectToServer()
+        {
+
+        }
     }
 }
